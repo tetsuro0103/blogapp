200b37c33347f18e10c1ab3ea221d61c71b138c6
泉徹郎 <>
2023-09-30 17:37:47 +0900

    タイムラインの実装

--- app/models/article.rb
+++ app/models/article.rb
@@ -27,16 +27,4 @@ class Article < ApplicationRecord
 
     belongs_to :user
 
-    def display_created_at
-        I18n.l(self.created_at, format: :default)
-    end
-
-    def author_name
-        user.display_name
-    end
-
-    def like_count
-        likes.count
-    end
-
 end
